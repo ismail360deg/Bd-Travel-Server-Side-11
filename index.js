28ba3bc35c6f0fcf424f403a6fcd0b2033e72047
ismail360deg <ismailbhuiyan2000@gmail.com>
2022-11-08 15:53:31 +0600

    username and password added

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/", (req, res) => {
 
 
 
-const uri = "mongodb+srv://<username>:<password>@cluster0.rgz3uon.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rgz3uon.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 client.connect(err => {
     const collection = client.db("test").collection("devices");
