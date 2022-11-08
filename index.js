f4c306f7a6f85d3c845a72ecd0298de4030380d3
ismail360deg <ismailbhuiyan2000@gmail.com>
2022-11-08 15:45:15 +0600

    mongodb added

--- index.js
+++ index.js
@@ -4,11 +4,26 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
+app.use(cors());
+app.use(express.json());
 
 
 app.get("/", (req, res) => {
     res.send("BD-Travel");
 });
+
+
+
+const uri = "mongodb+srv://<username>:<password>@cluster0.rgz3uon.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+    const collection = client.db("test").collection("devices");
+    // perform actions on the collection object
+    client.close();
+});
+
 
 
 app.listen(port, () => {
