2510c2761713ba40bd63c2765e7ffbce90068a7d
ismail360deg <ismailbhuiyan2000@gmail.com>
2022-11-09 22:02:39 +0600

    services findOne query added

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
@@ -18,14 +18,46 @@ app.get("/", (req, res) => {
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.rgz3uon.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
+
+
+
+
+async function run() {
+    try {
+        const servicesCollection = client.db("bdTravel").collection("services");
+
+
+        // read
+
+        app.get("/services", async (req, res) => {
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        });
+
+        app.get("/services/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const doubleSection = await servicesCollection.findOne(query);
+            res.send(doubleSection);
+        });
+
+    }
+    finally {
+
+    }
+}
+
+run().catch(err => console.error(err));
 
 
 
 app.listen(port, () => {
     console.log(`server is working from ${port}`);
 });
+
+
+
+
+// find(query).sort({date: -1})
